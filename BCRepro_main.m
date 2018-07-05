ed9c12f94bc5700b47c80a0c2c00d5c6d6e15c12
da5nsy <dannygarside@outlook.com>
2018-07-05 10:16:03 +0100

    add mel

--- BCRepro_main.m
+++ BCRepro_main.m
@@ -23,7 +23,7 @@ im = reflectances; clear reflectances
 % 21 D ills 3600:25000, no mention of interval so assuming the interval
 % which gives 21, though an interval of 1020 is weird, and I personally 
 % would think that a non-linear interval would make more sense.
-D_CCT=3600:1020:25000;
+D_CCT=3600:1020:25000; %but including [3940,5205,6677,24770]
 load B_cieday
 daylight_spd = GenerateCIEDay(D_CCT,[B_cieday]);
 for i=1:size(daylight_spd,2) %Normalise. Sure there's a more efficient way to do this with matrix division but I am tired.
@@ -31,19 +31,36 @@ for i=1:size(daylight_spd,2) %Normalise. Sure there's a more efficient way to do
 end
 
 % Observer(s)
-% Smith Pokorny from CVRL
+
+% Smith-Pokorny from CVRL
 T_sp = csvread('C:\Users\cege-user\Dropbox\UCL\Data\Colour standards\CVRL cone fundamentals\sp.csv');
 T_sp(:,2:4)=(10.^(T_sp(:,2:4)))./max(10.^(T_sp(:,2:4))); %Make non-log, and normalise to peak 1
 %Special normalization !!!!!!!!!!!!
-S_sp=[T_sp(1,1),T_sp(2,1)-T_sp(1,1),length(T_sp)];
-T_sp=T_sp(:,2:4);
+S_sp=[T_sp(1,1),T_sp(2,1)-T_sp(1,1),length(T_sp)]; %Get wavlength range, and put in psychtoolbox format
+T_sp=T_sp(:,2:4); %Remove wavelength range
 %plot(SToWls(S_sp),T_sp)
 
 % Psychtoolbox version
-% % Not using because of curtailment at low and high ends.
+% % Not using because of 0s at low and high ends.
 % % See note on http://docs.psychtoolbox.org/PsychColorimetricMatFiles
 % load T_cones_sp
 % plot(SToWls(S_cones_sp),T_cones_sp,'o')
+
+% Rods
+load T_rods
+
+% Melanopsin
+% Psychtoolbox's melanopsin function is neither the same as the Lucas+ 2014
+% data, which it claims to be in the docs, or the Enezi data which should
+% peak at 484. (PsychT = 488nm, Lucas = 490nm)
+% And so for simplicity, for now, I'll use psychtoolbox, but remember that
+% if the results come out slightly diff, this could be a contributor.
+load T_melanopsin 
+
+% figure, hold on
+% plot(SToWls(S_sp),T_sp)
+% plot(SToWls(S_rods),T_rods)
+% plot(SToWls(S_melanopsin),T_melanopsin)
 
 %% Calculate LMSRI for each pixel
 
