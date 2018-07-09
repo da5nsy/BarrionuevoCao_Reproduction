46479759b7d0db100999541f55f1faf0b096c472
da5nsy <dannygarside@outlook.com>
2018-07-09 20:54:40 +0100

    reproduced figure 3

--- BCRepro_caller.m
+++ BCRepro_caller.m
@@ -57,15 +57,12 @@ end
 % Reproduce figure 2
 
 % TO DO:
-% - check out the weird thing at the start (low CCT) of the third component
-% - add second level pcas
 % - add plot lines
 % - go back and add multiple images
 % - get legends in the right place
-% - add titles
 
-figure, hold on
-ylb={'1st Component Coefficients','2nd Component Coefficients','3rd Component Coefficients','4th Component Coefficients'};
+figure('units','normalized','outerposition',[0 0 0.5 1]), hold on
+ylb={'1st Component Coefficients','2nd Component Coefficients','3rd Component Coefficients','4th Component Coefficients','5th Component Coefficients'};
 
 for j=1:4
     subplot(4,2,j*2-1), hold on
@@ -108,6 +105,55 @@ title('B: Second Level Analysis')
 legend([h5 h6 h7],{'L/(L+M)','S/(L+M)','R/(L+M)'})
 
 
+% - %
+
+% Reproduce figure 3
+
+figure('units','normalized','outerposition',[0 0 0.5 1]), hold on
+
+for j=1:5
+    subplot(5,2,j*2-1), hold on
+    plot([0,D_CCT(end)],[0,0],'k:')
+    for i=1:length(res)
+        if (min(size(res(i).P_coeff) == [5,5])) && (res(i).level == 1)
+            h1 = scatter(res(i).CCT,res(i).P_coeff(1,j),'k','filled',                'MarkerEdgeColor',[0,0,0]);
+            h2 = scatter(res(i).CCT,res(i).P_coeff(2,j),'ks','filled',               'MarkerEdgeColor',[0,0,0]);        
+            h3 = scatter(res(i).CCT,res(i).P_coeff(3,j),'k^','filled',               'MarkerEdgeColor',[0,0,0]);              
+            h4 = scatter(res(i).CCT,res(i).P_coeff(4,j),'MarkerFaceColor',[1,1,1],   'MarkerEdgeColor',[0,0,0]);             
+            h5 = scatter(res(i).CCT,res(i).P_coeff(5,j),'v','MarkerFaceColor',[1,1,1],   'MarkerEdgeColor',[0,0,0]);
+        end
+    end
+    ylim([-1,1])
+    ylabel(ylb(1,j))    
+end
+xlabel('Illuminant CCT (K)')
+
+for j=1:4
+    subplot(5,2,j*2+2), hold on
+    plot([0,D_CCT(end)],[0,0],'k:')
+    for i=1:length(res)
+        if (min(size(res(i).P_coeff) == [4,4])) && (res(i).level == 2)
+            h6 = scatter(res(i).CCT,res(i).P_coeff(1,j),'k','filled',                'MarkerEdgeColor',[0,0,0]);
+            h7 = scatter(res(i).CCT,res(i).P_coeff(2,j),'k^','filled',               'MarkerEdgeColor',[0,0,0]);             
+            h8 = scatter(res(i).CCT,res(i).P_coeff(3,j),'MarkerFaceColor',[1,1,1],   'MarkerEdgeColor',[0,0,0]);
+            h9 = scatter(res(i).CCT,res(i).P_coeff(4,j),'v','MarkerFaceColor',[1,1,1],   'MarkerEdgeColor',[0,0,0]);
+        end
+    end
+    ylim([-1,1])
+    ylabel(ylb(1,j))    
+end
+xlabel('Illuminant CCT (K)')
+
+subplot(5,2,1)
+title('A: First Level Analysis')
+legend([h1 h2 h3 h4 h5],{'L','M','S','R','I'})
+
+
+subplot(5,2,4)
+title('B: Second Level Analysis')
+legend([h6 h7 h8 h9],{'L/(L+M)','S/(L+M)','R/(L+M)','I/(L+M)'})
+
+% - %
 
 % for i=1:length(res)
 %     if (min(size(res(i).P_coeff) == [3,3])) && (res(i).level == 1)
@@ -121,16 +167,12 @@ legend([h5 h6 h7],{'L/(L+M)','S/(L+M)','R/(L+M)'})
 
 %%
 
-% Compare with table 5 of original pub
-% It looks as though the third component might have the wrong signs, hmmm
-% otherwise everything else matches fairly well
-
-% from pub, for comparison
-table5 = [0.49, 0.48, 0.38, 0.45, 0.43;...
-    -0.40 -0.38, 0.80, -0.04, 0.20;...
-    0.59, -0.08, 0.39, -0.48, -0.48;...
-    -0.45, 0.64 0.21, 0.15, -0.54;...
-    0.18, -0.45, 0.03, 0.73, -0.48];
+% % from pub, for comparison
+% table5 = [0.49, 0.48, 0.38, 0.45, 0.43;...
+%     -0.40 -0.38, 0.80, -0.04, 0.20;...
+%     0.59, -0.08, 0.39, -0.48, -0.48;...
+%     -0.45, 0.64 0.21, 0.15, -0.54;...
+%     0.18, -0.45, 0.03, 0.73, -0.48];
 
 % format bank
 % P_coeff'-table5
