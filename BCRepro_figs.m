2f58d71df7f1fbc1f6b8a73bf2a501ec9ca6712f
da5nsy <dannygarside@outlook.com>
2020-07-11 19:51:44 -0400

    fix bug in plot saving

--- BCRepro_figs.m
+++ BCRepro_figs.m
@@ -15,15 +15,15 @@ function [f2,f3] = BCRepro_figs(res,D_CCT_range,imn)
 % - get legends in the right place
 % - change x-axis notation
 
-fs=10;
+fs = 10; % font size
 
 f2 = figure('units','normalized','outerposition',[0 0 0.4 1]); hold on
-ylb={'1st Component Coefficients','2nd Component Coefficients','3rd Component Coefficients','4th Component Coefficients','5th Component Coefficients'};
+ylb = {'1st Component Coefficients','2nd Component Coefficients','3rd Component Coefficients','4th Component Coefficients','5th Component Coefficients'};
 
-for j=1:4
+for j = 1:4
     subplot(4,2,j*2-1), hold on
     plot([0,D_CCT_range(end)],[0,0],'k:')
-    for i=1:length(res)
+    for i = 1:length(res)
         if (min(size(res(i).P_coeff) == [4,4])) && (res(i).level == 1) && (res(i).imn == imn)
             h1 = scatter(res(i).CCT,res(i).P_coeff(1,j),'k','filled',                'MarkerEdgeColor',[0,0,0]);
             h2 = scatter(res(i).CCT,res(i).P_coeff(2,j),'ks','filled',               'MarkerEdgeColor',[0,0,0]);        
@@ -36,10 +36,10 @@ for j=1:4
 end
 xlabel('Illuminant CCT (K)')
 
-for j=1:3
+for j = 1:3
     subplot(4,2,j*2+2), hold on
     plot([0,D_CCT_range(end)],[0,0],'k:')
-    for i=1:length(res)
+    for i = 1:length(res)
         if (min(size(res(i).P_coeff) == [3,3])) && (res(i).level == 2) && (res(i).imn == imn)
             h5 = scatter(res(i).CCT,res(i).P_coeff(1,j),'k','filled',                'MarkerEdgeColor',[0,0,0]);
             h6 = scatter(res(i).CCT,res(i).P_coeff(2,j),'k^','filled',               'MarkerEdgeColor',[0,0,0]);             
@@ -55,7 +55,6 @@ subplot(4,2,1)
 title('A: First Level Analysis','FontSize', fs)
 legend([h1 h2 h3 h4],{'L','M','S','R'})
 
-
 subplot(4,2,4)
 title('B: Second Level Analysis','FontSize', fs)
 legend([h5 h6 h7],{'L/(L+M)','S/(L+M)','R/(L+M)'})
@@ -67,10 +66,10 @@ legend([h5 h6 h7],{'L/(L+M)','S/(L+M)','R/(L+M)'})
 
 f3 = figure('units','normalized','outerposition',[0 0 0.4 1]); hold on
 
-for j=1:5
+for j = 1:5
     subplot(5,2,j*2-1), hold on
     plot([0,D_CCT_range(end)],[0,0],'k:')
-    for i=1:length(res)
+    for i = 1:length(res)
         if (min(size(res(i).P_coeff) == [5,5])) && (res(i).level == 1) && (res(i).imn == imn)
             h1 = scatter(res(i).CCT,res(i).P_coeff(1,j),'k','filled',                'MarkerEdgeColor',[0,0,0]);
             h2 = scatter(res(i).CCT,res(i).P_coeff(2,j),'ks','filled',               'MarkerEdgeColor',[0,0,0]);        
@@ -84,10 +83,10 @@ for j=1:5
 end
 xlabel('Illuminant CCT (K)','FontSize', fs)
 
-for j=1:4
+for j = 1:4
     subplot(5,2,j*2+2), hold on
     plot([0,D_CCT_range(end)],[0,0],'k:')
-    for i=1:length(res)
+    for i = 1:length(res)
         if (min(size(res(i).P_coeff) == [4,4])) && (res(i).level == 2) && (res(i).imn == imn)
             h6 = scatter(res(i).CCT,res(i).P_coeff(1,j),'k','filled',                'MarkerEdgeColor',[0,0,0]);
             h7 = scatter(res(i).CCT,res(i).P_coeff(2,j),'k^','filled',               'MarkerEdgeColor',[0,0,0]);             
@@ -103,7 +102,6 @@ xlabel('Illuminant CCT (K)','FontSize', fs)
 subplot(5,2,1)
 title('A: First Level Analysis','FontSize', fs)
 legend([h1 h2 h3 h4 h5],{'L','M','S','R','I'})
-
 
 subplot(5,2,4)
 title('B: Second Level Analysis','FontSize', fs)
